188af6dd1df99a7604eabb355573cb4cd3e9473d
bvoels <bradleyvoels@gmail.com>
2016-01-25 10:08:14 -0800

    Input Clear

--- javascripts.js
+++ javascripts.js
@@ -5,4 +5,5 @@ function othername() {
 	input = document.getElementById("userInput").value;
 	myyybase = myyybase + " " + input;
 	document.getElementById("list").innerHTML = myyybase;
+	document.getElementById("userInput").value = "";
 }
