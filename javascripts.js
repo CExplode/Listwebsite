e0740bba5cc6578cf3e8a2e08e3e068b7d86bf98
bvoels <bradleyvoels@gmail.com>
2016-02-18 09:33:55 -0800

    0.5.2.0
    Added a most common word search.

--- javascripts.js
+++ javascripts.js
@@ -26,6 +26,9 @@ function butType() {
 	document.getElementById("userInput").style.visibility = "visible";
 	document.getElementById("typeSubmit").style.visibility = "visible";
 	
+	document.getElementById("userWordParagraph").style.visibility = "hidden";
+	document.getElementById("userWordTest").style.visibility = "hidden";
+	document.getElementById("userWordSubmit").style.visibility = "hidden";
 	document.getElementById("list").style.visibility = "hidden";
 	document.getElementById("testArray").style.visibility = "hidden";
 }
@@ -33,21 +36,40 @@ function butType() {
 function butList() {
 	document.getElementById("list").style.visibility = "visible";
 	
+	document.getElementById("userWordParagraph").style.visibility = "hidden";
+	document.getElementById("userWordTest").style.visibility = "hidden";
+	document.getElementById("userWordSubmit").style.visibility = "hidden";
 	document.getElementById("userInput").style.visibility = "hidden";
 	document.getElementById("typeSubmit").style.visibility = "hidden";
 	document.getElementById("testArray").style.visibility = "hidden";
 	
-	document.getElementById("list").innerHTML = arrWords;
+	document.getElementById("list").innerHTML = "The current list of words is: " + arrWords;
 }
 
 function butAnalytics() {
+	document.getElementById("userWordTest").style.visibility = "visible";
+	document.getElementById("userWordSubmit").style.visibility = "visible";
 	document.getElementById("list").style.visibility = "visible";
+	document.getElementById("userWordParagraph").style.visibility = "visible";
 	displayArray();
 	
 	document.getElementById("userInput").style.visibility = "hidden";
 	document.getElementById("typeSubmit").style.visibility = "hidden";
 	document.getElementById("testArray").style.visibility = "hidden";
 }
+
+function searchWord() {
+	input = document.getElementById("userWordTest").value;
+	intCount = 0;
+	for(var i = 0; i < arrWords.length; i++){
+		if(input == arrWords[i]){
+			intCount++;
+		}
+	}
+	
+	document.getElementById("userWordParagraph").innerHTML = input + " has been typed in " + intCount + " times.";
+	document.getElementById("userWordTest").value = "";
+	}
 
 function displayArray() {
 	for(var i = 0; i < arrWords.length; i++)
@@ -72,6 +94,6 @@ function displayArray() {
             }
         }
 		
-        document.getElementById("list").innerHTML = intMode;	
+        document.getElementById("list").innerHTML = "The most common word is " + intMode;	
 	}
 }
